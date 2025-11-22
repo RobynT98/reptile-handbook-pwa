d3183a379f77aed7f0bc412e2c294ced0980249a
Conri Turesson <50335159+RobynT98@users.noreply.github.com>
2025-11-22 12:34:33 +0100

    Create src/data/species.ts

---
+++ src/data/species.ts
@@ -0,0 +1,6 @@
+import pythonRegius from './python_regius.json';
+import type { SpeciesProfile } from '../types/species';
+
+export const speciesList: SpeciesProfile[] = [
+  pythonRegius as SpeciesProfile
+];
