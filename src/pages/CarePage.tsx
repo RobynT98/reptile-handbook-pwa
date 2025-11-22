b48e153ab3845d8b3487e4de25662c7d23acc481
Conri Turesson <50335159+RobynT98@users.noreply.github.com>
2025-11-22 12:53:09 +0100

    Create src/pages/CarePage.tsx

---
+++ src/pages/CarePage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Card from '../components/Card';
+
+const CarePage: React.FC = () => (
+  <Card title="Vård &amp; Miljö">
+    <p>
+      Här hamnar guider om temperatur, luftfuktighet, terrariumupplägg,
+      karantän och generellt husbandry för reptiler &amp; exotiska djur.
+    </p>
+  </Card>
+);
+
+export default CarePage;
