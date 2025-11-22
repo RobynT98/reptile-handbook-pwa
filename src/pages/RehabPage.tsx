c6ee8ac5b5338c26bbb876b04e88b0c2b6b901e2
Conri Turesson <50335159+RobynT98@users.noreply.github.com>
2025-11-22 12:53:46 +0100

    Create src/pages/RehabPage.tsx

---
+++ src/pages/RehabPage.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Card from '../components/Card';
+
+const RehabPage: React.FC = () => (
+  <Card title="Rehab &amp; Rescue">
+    <p>
+      Här bygger vi protokoll för intag, karantän, vanliga skador/sjukdomar
+      och hur man arbetar med räddade reptiler på ett etiskt och
+      strukturerat sätt.
+    </p>
+  </Card>
+);
+
+export default RehabPage;
