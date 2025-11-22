48e58c51dd6ced3c97271d9fedbca5ec03a4b8f9
Conri Turesson <50335159+RobynT98@users.noreply.github.com>
2025-11-22 12:53:27 +0100

    Create src/pages/BreedingPage.tsx

---
+++ src/pages/BreedingPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Card from '../components/Card';
+
+const BreedingPage: React.FC = () => (
+  <Card title="Avel">
+    <p>
+      Här kommer avelssektionen: generella avelsguider, art-specifika
+      avelstips och struktur för att dokumentera kullar.
+    </p>
+  </Card>
+);
+
+export default BreedingPage;
