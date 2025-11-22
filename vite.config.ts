cac98f75a2d0a716d7ae209e82c92b518bc19ab4
Conri Turesson <50335159+RobynT98@users.noreply.github.com>
2025-11-22 13:40:24 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/reptile-handbook-pwa/', // viktigt: exakt ditt repo-namn
+  base: '/reptile-handbook-pwa/'
 });
